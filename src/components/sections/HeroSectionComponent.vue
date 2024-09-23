221b0ca9f5e6864f4c38cd32861574987a1b4b4a
edoriggio <edo.riggio19@gmail.com>
2024-09-23 20:02:32 +0200

    added countdown timer

--- src/components/sections/HeroSectionComponent.vue
+++ src/components/sections/HeroSectionComponent.vue
@@ -12,20 +12,59 @@
       and shape the future
     </span>
 
-    <div class="flex flex-col items-center gap-2">
-      <span class="text-center opacity-60">What are you waiting for?</span>
+    <div class="flex flex-col items-center gap-4">
+      <span class="text-center opacity-60">What are you waiting for? <br /> The registration will close in</span>
+
+      <div class="flex gap-2">
+        <div class="flex flex-col items-center">
+          <span class="text-2xl font-mono">{{timer.days}}</span>
+          <span class="text-xs text-gray-400">Days</span>
+        </div>
+
+        <div class="text-2xl font-mono">:</div>
+
+        <div class="flex flex-col items-center">
+          <span class="text-2xl font-mono">{{timer.hours}}</span>
+          <span class="text-xs text-gray-400">Hours</span>
+        </div>
+
+        <div class="text-2xl font-mono">:</div>
+
+        <div class="flex flex-col items-center">
+          <span class="text-2xl font-mono">{{timer.minutes}}</span>
+          <span class="text-xs text-gray-400">Minutes</span>
+        </div>
+
+        <div class="text-2xl font-mono">:</div>
+
+        <div class="flex flex-col items-center">
+          <span class="text-2xl font-mono">{{timer.seconds}}</span>
+          <span class="text-xs text-gray-400">Seconds</span>
+        </div>
+      </div>
+
       <cta-component href="https://usi.qualtrics.com/jfe/form/SV_bgCFvEjVFUvXq4u" variant="cta">Register Now!</cta-component>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { useTimer, UseTimer } from "vue-timer-hook";
 import CtaComponent from "@/components/CtaComponent.vue";
 
 export default {
   name: "HeroSectionComponent",
   components: {
     CtaComponent
+  },
+  data() {
+    return {
+      timer: {} as UseTimer,
+    }
+  },
+  mounted() {
+    const deadline = new Date('3 Oct 2024 23:59:59').toLocaleString('en-En', {timeZone: "Europe/Zurich"})
+    this.timer = useTimer(new Date(deadline).getTime(), true)
   }
 }
 </script>
